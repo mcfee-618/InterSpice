1f50d90770272f36fb8db68cb1a640aa5f04dd59
feipeixuan <feipeixuan@163.com>
2021-09-30 15:05:23 +0800

    complete post show function

--- article/urls.py
+++ article/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('post/list', views.list_post, name='list_post'),
+    path('post/list/', views.list_post, name='list_post'),
+    path('post/show/<int:post_id>', views.show_post, name='list_show'),
 ]
--- article/views.py
+++ article/views.py
@@ -1,5 +1,5 @@
 from django.core.paginator import Paginator
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views.decorators.http import require_http_methods
 from .models import Post, Category
 
@@ -8,13 +8,16 @@ def list_post(request):
     args = request.GET
     page = int(args.get("page", 1))
     posts = Post.objects.all().order_by('-timestamp')
-    categorys = Category.objects.all()
+    categories = Category.objects.all()
     paginator = Paginator(posts, 10)
     pageinfo = paginator.page(page)
-    context = {"pageinfo": pageinfo, "categories": categorys}
+    context = {"pageinfo": pageinfo, "categories": categories}
     return render(request, "article/list_post.html", context=context)
 
-
-def show_post(request):
-    pass
-
+@require_http_methods(["GET"])
+def show_post(request, post_id):
+    post = get_object_or_404(Post, pk=post_id)
+    categories = Category.objects.all()
+    comments = post.comment_set.all()
+    context = {"post": post, "categories": categories, "comments": comments}
+    return render(request, "article/show_post.html", context=context)
